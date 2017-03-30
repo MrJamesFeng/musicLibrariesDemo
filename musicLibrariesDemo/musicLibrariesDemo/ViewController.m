5816940fdff07eabb6be908fd0f71a1db13a46d1
MrJamesFeng <812463918@qq.co>
2017-03-30 18:01:36 +0800

    tijio

--- musicLibrariesDemo/musicLibrariesDemo/ViewController.m
+++ musicLibrariesDemo/musicLibrariesDemo/ViewController.m
@@ -7,22 +7,86 @@
 //
 
 #import "ViewController.h"
-
+#import <MediaPlayer/MediaPlayer.h>
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *controlSlider;
+@property (weak, nonatomic) IBOutlet UIButton *preSongBtn;
+@property (weak, nonatomic) IBOutlet UIButton *palyBtn;
+@property (weak, nonatomic) IBOutlet UIButton *nextSongBtn;
+@property (weak, nonatomic) IBOutlet UISlider *volumeSlider;
+
+@property(nonatomic,strong)NSTimer *timer;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    MPMusicPlayerController *musicPlayerVC;
+    MPVolumeView *volumeView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+   
+    UIView *backView = [[UIView alloc]initWithFrame:CGRectMake(100, 60, 100, 2)];
+     volumeView = [[MPVolumeView alloc]initWithFrame:backView.frame];
+    backView.backgroundColor = [UIColor grayColor];
+    [volumeView addSubview:backView];
+//    [self.volumeSlider removeFromSuperview];
+//    [volumeView addSubview:self.volumeSlider];
+//    [self.view addSubview:volumeView];
+    
+    musicPlayerVC = [[MPMusicPlayerController alloc]init];
+    [self registerNotification];//注册播放状态改变的通知
+}
+-(void)registerNotification{
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(playbackStateDidChangeAction:) name:MPMusicPlayerControllerPlaybackStateDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(nowPlayingItemDidChangeAction:) name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(volumeDidChangeAction:) name:MPMusicPlayerControllerVolumeDidChangeNotification object:nil];
+
+}
+#pragma notification actions
+-(void)playbackStateDidChangeAction:(NSNotification *)notification{
+    
+}
+-(void)nowPlayingItemDidChangeAction:(NSNotification *)notification{
+    
+}
+-(void)volumeDidChangeAction:(NSNotification *)notification{
+    
+}
+#pragma controls
+- (IBAction)controlSliderAction:(UISlider *)sender {
+    
+    
+    
+    
+}
+- (IBAction)volumeControlAction:(UISlider *)sender {
 }
 
+- (IBAction)preSongBtnAction:(UIButton *)sender {
+    [musicPlayerVC skipToPreviousItem];
+}
+- (IBAction)playBtnAction:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    if (sender.selected) {
+        [musicPlayerVC play];
+    }else if ([musicPlayerVC playbackState] == MPMusicPlaybackStatePlaying){
+        [musicPlayerVC pause];
+    }
+}
+- (IBAction)nextSongBtnAction:(UIButton *)sender {
+    [musicPlayerVC skipToNextItem];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
 }
 
 
